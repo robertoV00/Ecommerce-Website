0d2cae740aca710a4367d190a9b411b940abae05
Roberto Vega <droz.rv@gmail.com>
2025-03-05 21:29:44 -0500

    filter functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import Banner1 from "./components/Banner1"
-import Header from "./components/Header"
 import Nav from "./components/Nav"
-import Products from "./components/Products"
-import TrendingProducts from "./components/TrendingProducts"
 import { AppContext } from "./context/AppContext"
 import axios from "axios"
-import Banner2 from './components/Banner2';
 import Newsletter from './components/Newsletter';
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
 
 function App() {
 
@@ -34,7 +30,7 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/products" element={<Products />}/>
+          <Route path="/products" element={<ProductsPage />}/>
         </Routes>
         <Newsletter />
         <Footer />
